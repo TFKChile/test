ad6eabe78d7e13f1bd1229a2fac7a3c90ef6ac57
TFKChile <102984977+TFKChile@users.noreply.github.com>
2023-01-16 18:49:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(require("./src/routes/index"));
-app.use(express.static(path.join(__dirname, "public")));
-app.get("./src/public/index.html",(req,res)=>{
-        res.send("Hola")
-        })
+app.use(express.static(path.join(__dirname, "./src/public")));
 
 app.listen(3500,()=>{
     console.log("Server on port 3500")
